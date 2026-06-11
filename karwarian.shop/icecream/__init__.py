6d6a097b1502402c1d5eedbf325949b3a0c3d7d6
Tilak2001 <151338724+Tilak2001@users.noreply.github.com>
2026-06-11 22:24:17 +0530

    Delete karwarian.shop directory

--- karwarian.shop/icecream/__init__.py
+++
@@ -1,1 +0,0 @@
-# Anand Ice Cream app
--- karwarian.shop/jobs/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'jobs'
-
-urlpatterns = [
-    path('', views.jobs_list, name='list'),
-    path('<slug:slug>/', views.job_detail, name='detail'),
-]
